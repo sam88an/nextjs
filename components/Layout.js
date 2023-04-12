e1082df70ba50dcde3ed387e6e9a10ffef97672b
andv3 <andv@devcamp.edu.vn>
2023-04-12 10:45:16 +0700

    login

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,29 @@
+import { signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
-import { useSession } from 'next-auth/react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Link from 'next/link';
+import Cookies from 'js-cookie';
 import React, { useContext, useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { Menu } from '@headlessui/react';
+import 'react-toastify/dist/ReactToastify.css';
 import { Store } from '../utils/Store';
+import DropdownLink from './DropdownLink';
+
 export default function Layout({ title, children }) {
-const { status, data: session } = useSession();
-  
-  const { state } = useContext(Store);
+  const { status, data: session } = useSession();
+
+  const { state, dispatch } = useContext(Store);
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
   useEffect(() => {
     setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
   }, [cart.cartItems]);
+
+  const logoutClickHandler = () => {
+    Cookies.remove('cart');
+    dispatch({ type: 'CART_RESET' });
+    signOut({ callbackUrl: '/login' });
+  };
   return (
     <>
       <Head>
@@ -21,14 +31,16 @@ const { status, data: session } = useSession();
         <meta name="description" content="Ecommerce Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <ToastContainer position="bottom-center" limit={1} />
+
       <div className="flex min-h-screen flex-col justify-between ">
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
             <Link href="/">
               <span className="text-lg font-bold">amazona</span>
             </Link>
-            <div className='flex'>
+            <div>
               <Link href="/cart">
                 <span className="p-2">
                   Cart
@@ -39,16 +51,42 @@ const { status, data: session } = useSession();
                   )}
                 </span>
               </Link>
-              <Link href="/login">
-                <span className="p-2">Login</span>
-              </Link>
+
               {status === 'loading' ? (
                 'Loading'
               ) : session?.user ? (
-                session.user.name
+                <Menu as="div" className="relative inline-block">
+                  <Menu.Button className="text-blue-600">
+                    {session.user.name}
+                  </Menu.Button>
+                  <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white  shadow-lg ">
+                    <Menu.Item>
+                      <DropdownLink className="dropdown-link" href="/profile">
+                        Profile
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <DropdownLink
+                        className="dropdown-link"
+                        href="/order-history"
+                      >
+                        Order History
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <span
+                        className="dropdown-link"
+                        href="#"
+                        onClick={logoutClickHandler}
+                      >
+                        Logout
+                      </span>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
               ) : (
                 <Link href="/login">
-                  <a className="p-2">Login</a>
+                  <span className="p-2">Login</span>
                 </Link>
               )}
             </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
-import '@/styles/globals.css'
-import { StoreProvider } from '@/utils/Store'
+import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+import { StoreProvider } from '../utils/Store';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-    <StoreProvider>
-      <Component {...pageProps} />
-    </StoreProvider>
-  </SessionProvider>
-  
-  )
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
+    </SessionProvider>
+  );
 }
+
+export default MyApp;
