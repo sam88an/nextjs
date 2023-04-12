0141a1d3c4314f10e2830ed54c7c9fe1bbd9f83e
andv3 <andv@devcamp.edu.vn>
2023-04-12 09:52:07 +0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import { StoreProvider } from '@/utils/Store'
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  </SessionProvider>
+  
+  )
+}
